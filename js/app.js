9da3c4be9f585c1838ba0c763c6411ed93a5e0ae
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2022-03-30 18:49:31 +0530

    Add type writer animation

--- js/app.js
+++ js/app.js
@@ -1,9 +1,32 @@
 var input = document.getElementById('txt-input');
 var output = document.getElementById('output-text');
 
+var i = 0;
+var j = 0;
+const fps = 60;
+
+function animateText(){
+    
+    if (input.value){
+
+        output.innerText = input.value.substring(0,i);
+
+        if (j++ >= 60){
+            j = 0;
+            i++;
+
+            if (i > (input.value.length)) i = 0;
+        }
+
+        requestAnimationFrame(animateText);
+    }
+}
+
+
 input.addEventListener('input', function(){
     if (input.value){
-        output.innerText = input.value;
+        // output.innerText = input.value;
+        requestAnimationFrame(animateText);
     }else{
         output.innerText = 'Type Something 😉';
     }
